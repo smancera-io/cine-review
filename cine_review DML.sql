6ccef9381a4bdf9bc75279ba1bfa88ef93cb61ec
Samuel Mancera <samuelmancera.097@gmail.com>
2026-05-15 16:15:26 -0500

    feat: add initial data for Country

--- cine_review DML.sql
+++ cine_review DML.sql
@@ -1,3 +1,14 @@
 SET @co = UUID(); SET @us = UUID(); SET @kr = UUID();
 SET @uk = UUID(); SET @fr = UUID(); SET @mx = UUID();
 SET @ca = UUID(); SET @jp = UUID(); SET @de = UUID();
+
+INSERT INTO country (id, name) VALUES
+(@co, 'Colombia'),
+(@us, 'Estados Unidos'),
+(@kr, 'Corea del Sur'),
+(@uk, 'Reino Unido'),
+(@fr, 'Francia'),
+(@mx, 'México'),
+(@ca, 'Canadá'),
+(@jp, 'Japón'),
+(@de, 'Alemania');
