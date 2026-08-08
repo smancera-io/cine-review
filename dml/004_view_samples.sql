7ecf736e2ec59f0066d6face3a764a9bfa09490d
Samuel Mancera <samuelmancera.097@gmail.com>
2026-08-08 18:39:52 -0500

    docs: add view samples requirements and suggestions 'dml/004'

--- dml/004_view_samples.sql
+++ dml/004_view_samples.sql
@@ -1,4 +1,10 @@
-/* View queries */
+/*
+*   Sample queries against the 7 views defined in ddl/003_views.sql.
+*   See that file for what each view computes and why.
+*
+*   Run after dml/001_seed_data.sql and dml/003_procedures_driven_data.sql have populated the schema. Otherwise these return empty or near-empty result sets, 
+*   not errors.
+*/
 SELECT * FROM v_movie_ranking;
 SELECT * FROM v_genre_popularity;
 SELECT * FROM v_watchlist_popularity;
